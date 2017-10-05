80e04265419a138d3224177e66a392b0a8bd0ec2
student <s518807@nw42674.nwmissouri.edu>
2017-10-05 17:59:49 -0500

    Changed secondviewcontroller to ImportantDates

--- ProTeam/ImportantDatesController.swift
+++ ProTeam/ImportantDatesController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ImportantDatesController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
