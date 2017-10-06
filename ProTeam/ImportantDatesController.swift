e665fc1062cb9dadaae08d8fe5116efabf936744
student <s528754@ch140007m.nwmissouri.edu>
2017-10-05 19:12:43 -0500

    updated stubs in controllers

--- ProTeam/ImportantDatesController.swift
+++ ProTeam/ImportantDatesController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ImportantDatesController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int { // now we have 2 sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
 }
 
