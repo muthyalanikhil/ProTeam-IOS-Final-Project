deb2bd7a0640d88dc6abcf7fa473097e0fb3fe55
student <s518807@ch140011m.nwmissouri.edu>
2017-11-02 19:48:29 -0500

    ??

--- ProTeam/ProjectModel.swift
+++ ProTeam/ProjectModel.swift
@@ -6,16 +6,40 @@
 //  Copyright © 2017 Picklo,Blake H. All rights reserved.
 //
 
-import Foundation
 
-class project {
-    var name:String
-    var projClass:String
-    var dueDate:String
+import Foundation
+import CoreLocation
+import UIKit
+
+class Project:Equatable,CustomStringConvertible{
+    static func ==(lhs: Project, rhs: Project) -> Bool {
+        return true
+    }
     
-    init (name:String, projClass:String, dueDate:String) {
+    
+    var description:String{
+        return ""
+    }
+    
+    var name:String?
+    var endDate:DateComponents?
+    var startDate:DateComponents?
+    
+    
+    init(name:String, endDate:DateComponents,startDate:DateComponents){
         self.name = name
-        self.projClass = projClass
-        self.dueDate = dueDate
+        self.endDate = endDate
+        self.startDate = startDate
     }
 }
+
+var project1 = Project(name: "IOS - Project",endDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34), startDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34))
+var project2 = Project(name: "Project Management",endDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34),startDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34))
+var project3 = Project(name: "NWMSU",endDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34),startDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34))
+var project4 = Project(name: "Check Please",endDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34),startDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34))
+var project5 = Project(name: "Missouri Birders App",endDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34),startDate: DateComponents(year: 2013, month: 09, day:12, hour: 6, minute: 34))
+
+struct CurrentProject{
+    static var currentProjects:[Project] = [project1,project2,project3,project4,project5]
+}
+
