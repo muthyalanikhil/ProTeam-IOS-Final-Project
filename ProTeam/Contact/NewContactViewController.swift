344af2b8c05f0ee22a4762e87b6f388ae2aff6af
Meier,Jesse A <s518807@nw40383.nwmissouri.edu>
2017-12-03 18:31:53 -0600

    Fixed the add buttons

--- ProTeam/Contact/NewContactViewController.swift
+++ ProTeam/Contact/NewContactViewController.swift
@@ -21,19 +21,16 @@ class NewContactViewController: UIViewController {
         ContactsList.addNewContact(temp)
         self.dismiss(animated: true, completion: nil)
     }
-    @IBAction func DeleteBTN(_ sender: Any) {
-    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
-    
 }
 
---
+++ ProTeam/Projects/ProjectList.swift
@@ -0,0 +1,9 @@
+//
+//  ProjectList.swift
+//  ProTeam
+//
+//  Created by Meier,Jesse A on 12/3/17.
+//  Copyright © 2017 Picklo,Blake H. All rights reserved.
+//
+
+import Foundation
