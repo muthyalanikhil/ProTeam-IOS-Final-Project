bff2dedf1f6fb7538be74e93bbbd0e00e4a25023
student <s518807@ch140012m.nwmissouri.edu>
2017-12-06 19:23:37 -0600

    fixed picker view

--- ProTeam/Contact/NewContactViewController.swift
+++ ProTeam/Contact/NewContactViewController.swift
@@ -8,54 +8,53 @@
 
 import UIKit
     
-    class NewContactViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-        
-        @IBOutlet weak var nameTF: UITextField!
-        @IBOutlet weak var emailTF: UITextField!
-        @IBOutlet weak var pNumTF: UITextField!
-        var ProjectIn:String = ""
-        
-        @IBAction func cancelBTN(_ sender: Any) {
-            self.dismiss(animated: true, completion: nil)
-        }
-        
-        @IBAction func DoneBTN(_ sender: Any) {
-            let temp = Contact(name: nameTF.text!, PhoneNumber: pNumTF.text!, email: emailTF.text!, projIn:ProjectIn)
-            for project in ProjectsList.projects {
-                if project.name == ProjectIn {
-                    project.members.append(nameTF.text!)
-                }
+class NewContactViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var pNumTF: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var ProjectIn:String = ""
+    
+    @IBAction func cancelBTN(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func DoneBTN(_ sender: Any) {
+        let temp = Contact(name: nameTF.text!, PhoneNumber: pNumTF.text!, email: emailTF.text!, projIn:ProjectIn)
+        for project in ProjectsList.projects {
+            if project.name == ProjectIn {
+                project.members.append(nameTF.text!)
             }
-            ContactsList.addNewContact(temp)
-            self.dismiss(animated: true, completion: nil)
         }
+        ContactsList.addNewContact(temp)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ProjectsList.projects[row].name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ProjectsList.projects.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        ProjectIn = ProjectsList.projects[row].name
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
         
-        @IBOutlet weak var projectPicker: UIPickerView!
-        
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            return ProjectsList.projects[row].name
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return ProjectsList.projects.count
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-            ProjectIn = ProjectsList.projects[row].name
-        }
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-        }
-        
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            
-        }
+    }
 }
 
